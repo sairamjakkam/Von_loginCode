5eccf2f846b19da18fcec97fc18971e263b059b0
sairam <sairam.jakkam@techolution.com>
2025-11-04 16:25:16 +0530

    headless is true nows

--- src/test/steps/vonage.ts
+++ src/test/steps/vonage.ts
@@ -11,8 +11,8 @@ Given('I am on the login page', async function () {
 });
 
 When('I enter my username and password', async function () {
-    await pageFixture.page.fill(vonageLoginPage.locators.usernameInput, process.env.USERNAME);
-    await pageFixture.page.fill(vonageLoginPage.locators.passwordInput, process.env.PASSWORD);
+    await pageFixture.page.fill(vonageLoginPage.locators.usernameInput, data.credentials.username);
+    await pageFixture.page.fill(vonageLoginPage.locators.passwordInput, data.credentials.password);
 });
 
 When('I click the login button', async function () {
